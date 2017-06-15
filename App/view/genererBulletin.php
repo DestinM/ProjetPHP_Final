9d710809d8ff413860e32472f9453fcf701fe3e6
Jean Mike Luigi <luigi1528@gmail.com>
2017-06-15 08:21:14 -0500

    update generation formulaire

--- App/view/genererBulletin.php
+++ App/view/genererBulletin.php
@@ -5,8 +5,69 @@
 </head>
 <body>
 
-<h1>hello</h1>
+<h1 style="color: red;">generation du formulaire ... </h1>
+<?php
 
+try
+
+{
+
+    // On se connecte à MySQL
+
+/*    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');*/
+    require_once("../model/Database.php");
+
+    $database = new Database();
+    $conn = $database->connect();
+
+}
+
+catch(Exception $e)
+
+{
+
+    // En cas d'erreur, on affiche un message et on arrête tout
+
+        die('Erreur : '.$e->getMessage());
+
+}
+
+
+// Si tout va bien, on peut continuer
+
+
+// On récupère tout le contenu de la table jeux_video
+
+$reponse = $conn->query('SELECT matiere, note FROM etudiant_note');
+
+$donnees = $reponse->fetchAll();
+echo "<pre>";
+print_r($donnees[1]->matiere);
+echo "</pre>";
+?>
+
+<table>
+	<tbody>
+	<?php for ($i=0; $i < count($donnees); $i++) { ?>
+
+		<tr>
+			<td><?php echo $donnees[$i]->matiere; ?></td>
+			<td><?php echo $donnees[$i]->note; ?></td>
+		</tr>
+	<?php } ?>
+	</tbody>
+</table>
+
+
+<?php
+
+
+
+
+$reponse->closeCursor(); // Termine le traitement de la requête
+
+
+ ?>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.debug.js"></script>
 
@@ -17,7 +78,14 @@
     var prenom = <?php echo $_GET['prenom']; ?>;
 
 //    doc.text('Hello world!', 10, 10);
-    impression = nom + " " + prenom;
+    impression = nom + " " + prenom + "\n";
+
+	<?php for ($i=0; $i < count($donnees); $i++) { ?>
+
+	//alert(impression);
+	impression += '<?php echo $donnees[$i]->matiere . " : " . $donnees[$i]->note ; ?>\n';
+
+	<?php } ?>
 
     doc.text(impression, 10, 10);
     //alert(impression);
